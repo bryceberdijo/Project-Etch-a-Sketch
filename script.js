6112ad8b278a38967ab76c5927aaa83e00a9cbf0
Bryce <240708363+bryceberdijo@users.noreply.github.com>
2026-06-13 16:13:56 +0800

    I have added the div contrainers with a basic styles setting to it. Tho, I am still unsure on how I would do the rest

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+const container = document.querySelector(".container");
+
+const row1 = document.createElement("div");
+const row2 = document.createElement("div");
+const row3 = document.createElement("div");
+//row1.classList.add('container');
+row1.id = "container2";
+row2.id = "container2";
+row3.id = "container2";
+
+container.appendChild(row1);
+container.appendChild(row2);
+container.appendChild(row3);
