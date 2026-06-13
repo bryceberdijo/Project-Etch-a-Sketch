360f3a0cf26b57162789aafd239ff35f69b514de
Bryce <240708363+bryceberdijo@users.noreply.github.com>
2026-06-13 16:58:46 +0800

    Added the 16x16 squares as required by the odin project description

--- script.js
+++ script.js
@@ -2,14 +2,19 @@
 
 const container = document.querySelector(".container");
 
-const row1 = document.createElement("div");
-const row2 = document.createElement("div");
-const row3 = document.createElement("div");
-//row1.classList.add('container');
-row1.id = "container2";
-row2.id = "container2";
-row3.id = "container2";
 
-container.appendChild(row1);
-container.appendChild(row2);
-container.appendChild(row3);
+
+
+for (let i = 0; i < 16; i++) {
+    const row1 = document.createElement("div");
+    row1.id = "container2";
+    container.appendChild(row1);
+    console.log(i)
+    for (let i = 0; i < 16; i++) {
+        const row2 = document.createElement("div");
+        row2.id = "container3";
+        row1.appendChild(row2);
+        console.log(i)
+    
+}
+}
